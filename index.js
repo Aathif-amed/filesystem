75b6eac1dd1e5e7ccc577d4097360f2c7835cd7c
Aathif-amed <92617395+Aathif-amed@users.noreply.github.com>
2022-11-20 18:50:25 +0530

    completed

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+const currrentTime = Date.now();
+
+console.log(currrentTime);
+
+
+app.get("/file", (req, res) => {
+  fs.writeFile(`./timestamps/${currrentTime}.txt`, `${currrentTime}`, (err) => {
+    console.log(err);
+    console.log("Completed writing !!! 👍");
+    console.log(currrentTime);
+    res.send(`Created ${currrentTime}.txt`);
+  });
+});
+
+app.get("/read", async (req, res) => {
+  fs.readdir(`./timestamps`, "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(data);
+  });
+});
+
+app.listen(3004);
